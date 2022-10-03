68908ca52dcc7618580da0810469487ac12bacf5
Nicolas Carvalho <nicolas.carvalho@hotmail.com.br>
2022-10-03 14:53:19 -0300

    Page home added

---
+++ src/pages/Home.js
@@ -0,0 +1,63 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Ignite</Text>
+      <Text style={styles.title}>Welcome, Nicolas!</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="New Skill"
+        placeholderTextColor="#555"
+      />
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+
+      <Text style={[styles.title, {marginTop: 20}]}>My Skills</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 20,
+    paddingVertical: 70,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#1F1E25',
+    color: '#FFF',
+    fontSize: 18,
+    marginTop: 30,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    borderRadius: 7,
+  },
+  button: {
+    backgroundColor: '#A370F7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+});
